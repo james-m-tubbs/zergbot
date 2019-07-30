4b4a18e7db154425be7cc61e9aabdb8f1f0a8003
James Mak-Tubbs <james.m.tubbs@gmail.com>
2019-07-30 11:18:54 -0400

    smarten up branch

--- bots/zergbot_main.py
+++ bots/zergbot_main.py
@@ -57,7 +57,7 @@ basic_actions = [
 
 ACTION_SELECT_HATCHERY = 'selecthatchery'
 ACTION_SELECT_LARVA = 'selectlarva'
-ACTION_TRAIN_QUEEN = 'selectlarva'
+ACTION_TRAIN_QUEEN = 'trainqueen'
 ACTION_TRAIN_DRONE = 'traindrone'
 ACTION_SELECT_DRONE = 'selectdrone'
 ACTION_TRAIN_OVERLORD = 'trainoverlord'
@@ -75,20 +75,42 @@ hatchery_actions = [
 
 ACTION_BUILD_SPAWNING_POOL = 'buildspawningpool'
 ACTION_TRAIN_ZERGLING = 'trainzergling'
+ACTION_SELECT_ZERGLING = 'selectzergling'
+ACTION_SELECT_SPAWNING_POOL = 'selectspawningpool'
+ACTION_RESEARCH_ADRENAL_GLANDS = 'researchadrenalglands'
+ACTION_RESEARCH_METABOLIC_BOOST = 'researchmetabolicboost'
 
 zergling_actions = [
     ACTION_BUILD_SPAWNING_POOL,
-    ACTION_TRAIN_ZERGLING
+    ACTION_TRAIN_ZERGLING,
+    ACTION_SELECT_SPAWNING_POOL,
+    ACTION_RESEARCH_ADRENAL_GLANDS,
+    ACTION_RESEARCH_METABOLIC_BOOST
 ]
 
 smart_actions = basic_actions + hatchery_actions + zergling_actions
 
+#####################
+### Reward Values ###
+#####################
+
 KILL_UNIT_REWARD = 0.2
-KILL_BUILDING_REWARD = 0.5
+KILL_BUILDING_REWARD = 0.6
+
+TOTAL_VALUE_UNITS_REWARD = 0.5
+TOTAL_VALUE_STRUCTURES_REWARD = 0.5
+
+MINERAL_COLLECTION_REWARD = 0.7
+VESPENE_COLLECTION_REWARD = 0.7
+MINERAL_SPENT_REWARD = 0.4
+VESPENE_SPENT_REWARD = 0.4
+DRONE_CREATE_REWARD = 0.3
+
+ZERGLING_CREATE_REWARD = 1
 
 # Stolen from https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow
 class QLearningTable:
-  def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+  def __init__(self, actions, learning_rate=0.1, reward_decay=0.8, e_greedy=0.9):
     self.actions = actions  # a list
     self.lr = learning_rate
     self.gamma = reward_decay
@@ -182,6 +204,14 @@ class ZergAgent(base_agent.BaseAgent):
     killed_unit_score = obs.observation['score_cumulative'][5]
     killed_building_score = obs.observation['score_cumulative'][6]
 
+    total_value_units = obs.observation['score_cumulative'][3]
+    total_value_structures = obs.observation['score_cumulative'][4]
+
+    mineral_collection_rate = obs.observation['score_cumulative'][9]
+    vespene_collection_rate = obs.observation['score_cumulative'][10]
+    minerals_spent = obs.observation['score_cumulative'][11]
+    vespene_spent = obs.observation['score_cumulative'][12]
+
     ##### capture current game state #####
     supply_limit = obs.observation['player'][4]
     army_supply = obs.observation['player'][5]
@@ -193,6 +223,7 @@ class ZergAgent(base_agent.BaseAgent):
     overlord_count = len(self.get_units_by_type(obs, units.Zerg.Overlord))
     queen_count = len(self.get_units_by_type(obs, units.Zerg.Queen))
     zergling_count = len(self.get_units_by_type(obs, units.Zerg.Zergling))
+    drone_count = len(self.get_units_by_type(obs, units.Zerg.Drone))
     larva_count = len(self.get_units_by_type(obs, units.Zerg.Larva))
 
     # build the state object
@@ -204,6 +235,11 @@ class ZergAgent(base_agent.BaseAgent):
       spawning_pool_count,
       supply_limit,
       army_supply,
+      drone_count,
+      mineral_collection_rate,
+      minerals_spent,
+      vespene_collection_rate,
+      vespene_spent
     ]
 
     if self.previous_action is not None:
@@ -215,6 +251,36 @@ class ZergAgent(base_agent.BaseAgent):
       if killed_building_score > self.previous_killed_building_score:
         reward += KILL_BUILDING_REWARD
 
+      if mineral_collection_rate > self.prevoius_mineral_collection_rate:
+          reward += MINERAL_COLLECTION_REWARD
+
+      if mineral_collection_rate < self.prevoius_mineral_collection_rate:
+          reward -= MINERAL_COLLECTION_REWARD
+
+      if vespene_collection_rate > self.prevoius_vespene_collection_rate:
+          reward += VESPENE_COLLECTION_REWARD
+
+      if vespene_collection_rate > self.prevoius_vespene_collection_rate:
+          reward -= VESPENE_COLLECTION_REWARD
+
+      if minerals_spent > self.prevoius_minerals_spent:
+          reward += MINERAL_SPENT_REWARD
+
+      if vespene_spent > self.prevoius_vespene_spent:
+        reward += VESPENE_SPENT_REWARD
+
+      if total_value_units > self.previous_total_value_units:
+        reward += TOTAL_VALUE_UNITS_REWARD
+
+      if total_value_structures > self.previous_total_value_structures:
+          reward += TOTAL_VALUE_STRUCTURES_REWARD
+
+      if drone_count != self.previous_drone_count:
+        reward += DRONE_CREATE_REWARD * (drone_count - self.previous_drone_count)
+
+      if zergling_count != self.previous_zergling_count:
+          reward += ZERGLING_CREATE_REWARD * (zergling_count - self.previous_zergling_count)
+
       self.qlearn.learn(str(self.previous_state), self.previous_action, reward, str(current_state))
 
     rl_action = self.qlearn.choose_action(str(current_state))
@@ -222,6 +288,20 @@ class ZergAgent(base_agent.BaseAgent):
 
     self.previous_killed_unit_score = killed_unit_score
     self.previous_killed_building_score = killed_building_score
+
+    self.previous_total_value_units = total_value_units
+    self.previous_total_value_structures = total_value_structures
+
+    self.prevoius_mineral_collection_rate = mineral_collection_rate
+    self.prevoius_vespene_collection_rate = vespene_collection_rate
+
+    self.prevoius_minerals_spent = minerals_spent
+    self.prevoius_vespene_spent = vespene_spent
+
+    self.previous_drone_count = drone_count
+
+    self.previous_zergling_count = zergling_count
+
     self.previous_state = current_state
     self.previous_action = rl_action
 
@@ -233,16 +313,32 @@ class ZergAgent(base_agent.BaseAgent):
     if smart_action == ACTION_DO_NOTHING:
         return actions.FunctionCall(_NO_OP, [])
 
+    elif smart_action == ACTION_SELECT_ARMY:
+        zerglings = self.get_units_by_type(obs, units.Zerg.Zergling)
+        if len(zerglings) > 1:
+            zergling = random.choice(zerglings)
+            return actions.FUNCTIONS.select_point("select_all_type", (zergling.x,
+                                                                      zergling.y))
+
+    elif smart_action == ACTION_ATTACK:
+        zerglings = self.get_units_by_type(obs, units.Zerg.Zergling)
+        if len(zerglings) >= 10:
+            if self.unit_type_is_selected(obs, units.Zerg.Zergling):
+                if self.can_do(obs, actions.FUNCTIONS.Attack_minimap.id):
+                    return actions.FUNCTIONS.Attack_minimap("now",
+                                                            self.attack_coordinates)
+
     ###############################
     ### Hatchery Actions ###
     ###############################
 
     elif smart_action == ACTION_SELECT_LARVA:
-      larvae = self.get_units_by_type(obs, units.Zerg.Larva)
-      if len(larvae) > 0:
-        larva = random.choice(larvae)
-        return actions.FUNCTIONS.select_point("select_all_type", (larva.x,
+        larvae = self.get_units_by_type(obs, units.Zerg.Larva)
+        if len(larvae) > 0:
+          larva = random.choice(larvae)
+          return actions.FUNCTIONS.select_point("select_all_type", (larva.x,
                                                                   larva.y))
+
     elif smart_action == ACTION_SELECT_DRONE:
         drones = self.get_units_by_type(obs, units.Zerg.Drone)
         if len(drones) > 0:
@@ -255,21 +351,31 @@ class ZergAgent(base_agent.BaseAgent):
       if free_supply == 0:
         if self.can_do(obs, actions.FUNCTIONS.Train_Overlord_quick.id):
           return actions.FUNCTIONS.Train_Overlord_quick("now")
+        else:
+            larvae = self.get_units_by_type(obs, units.Zerg.Larva)
+            if len(larvae) > 0:
+                larva = random.choice(larvae)
+                return actions.FUNCTIONS.select_point("select_all_type", (larva.x,
+                                                                          larva.y))
 
     elif smart_action == ACTION_TRAIN_DRONE:
         drones = self.get_units_by_type(obs, units.Zerg.Drone)
         if len(drones) < 16:
           if self.can_do(obs, actions.FUNCTIONS.Train_Drone_quick.id):
             return actions.FUNCTIONS.Train_Drone_quick("now")
+          else:
+            larvae = self.get_units_by_type(obs, units.Zerg.Larva)
+            if len(larvae) > 0:
+                larva = random.choice(larvae)
+                return actions.FUNCTIONS.select_point("select_all_type", (larva.x,
+                                                                          larva.y))
 
     elif smart_action == ACTION_SELECT_HATCHERY:
-        hatcheries = self.get_units_by_type(obs, units.Zerg.Hatchery)
+      hatcheries = self.get_units_by_type(obs, units.Zerg.Hatchery)
+      if len(hatcheries) > 0:
         hatchery = random.choice(hatcheries)
-        return actions.FUNCTIONS.select_point("select_all_type", (hatchery.x, hatchery.y))
-
-    elif smart_action == ACTION_TRAIN_QUEEN:
-        if self.can_do(obs, actions.FUNCTIONS.Train_Queen_quick.id):
-            return actions.FUNCTIONS.Train_Queen_quick("now")
+        return actions.FUNCTIONS.select_point("select_all_type", (hatchery.x,
+                                                                  hatchery.y))
 
     #######################
     ### Zerging Actions ###
@@ -283,10 +389,62 @@ class ZergAgent(base_agent.BaseAgent):
                 y = random.randint(0, 83)
 
                 return actions.FUNCTIONS.Build_SpawningPool_screen("now", (x, y))
+        else:
+          drones = self.get_units_by_type(obs, units.Zerg.Drone)
+          if len(drones) > 0:
+            drone = random.choice(drones)
+            return actions.FUNCTIONS.select_point("select_all_type", (drone.x,
+                                                                      drone.y))
+      else:
+          larvae = self.get_units_by_type(obs, units.Zerg.Larva)
+          if len(larvae) > 0:
+              larva = random.choice(larvae)
+              return actions.FUNCTIONS.select_point("select_all_type", (larva.x,
+                                                                        larva.y))
+
+
+
+    elif smart_action == ACTION_TRAIN_QUEEN:
+        if len(self.get_units_by_type(obs, units.Zerg.Queen)) < 2*(len(self.get_units_by_type(obs, units.Zerg.Hatchery))):
+          if self.can_do(obs, actions.FUNCTIONS.Train_Queen_quick.id):
+            return actions.FUNCTIONS.Train_Queen_quick("now")
+          else:
+            hatcheries = self.get_units_by_type(obs, units.Zerg.Hatchery)
+            if len(hatcheries) > 0:
+                hatchery = random.choice(hatcheries)
+                return actions.FUNCTIONS.select_point("select_all_type", (hatchery.x,
+                                                                          hatchery.y))
 
     elif smart_action == ACTION_TRAIN_ZERGLING:
         if self.can_do(obs, actions.FUNCTIONS.Train_Zergling_quick.id):
             return actions.FUNCTIONS.Train_Zergling_quick("now")
+        else:
+            larvae = self.get_units_by_type(obs, units.Zerg.Larva)
+            if len(larvae) > 0:
+                larva = random.choice(larvae)
+                return actions.FUNCTIONS.select_point("select_all_type", (larva.x,
+                                                                          larva.y))
+
+    elif smart_action == ACTION_SELECT_SPAWNING_POOL:
+        spawning_pools = self.get_units_by_type(obs, units.Zerg.SpawningPool)
+        if len(spawning_pools) > 1:
+            spawning_pool = random.choice(spawning_pools)
+            return actions.FUNCTIONS.select_point("select_all_type", (spawning_pool.x,
+                                                                      spawning_pool.y))
+
+    elif smart_action == ACTION_RESEARCH_ADRENAL_GLANDS:
+      if self.unit_type_is_selected(obs, units.Zerg.Drone):
+        if self.can_do(obs, actions.FUNCTIONS.Research_ZerglingAdrenalGlands_quick.id):
+          return actions.FUNCTIONS.Research_ZerglingAdrenalGlands_quick("now")
+
+    elif smart_action == ACTION_RESEARCH_METABOLIC_BOOST:
+        if self.unit_type_is_selected(obs, units.Zerg.Drone):
+            if self.can_do(obs, actions.FUNCTIONS.Research_ZerglingMetabolicBoost_quick.id):
+                return actions.FUNCTIONS.Research_ZerglingMetabolicBoost_quick("now")
+
+    #######################
+    ### Default NO_OP   ###
+    #######################
 
     return actions.FunctionCall(_NO_OP, [])
 
@@ -300,7 +458,7 @@ def main(unused_argv):
                    sc2_env.Bot(sc2_env.Race.random,
                                sc2_env.Difficulty.very_easy)],
           agent_interface_format=features.AgentInterfaceFormat(
-              feature_dimensions=features.Dimensions(screen=84, minimap=64),
+              feature_dimensions=features.Dimensions(screen=168, minimap=128),
               use_feature_units=True),
           step_mul=16,
           game_steps_per_episode=0,
